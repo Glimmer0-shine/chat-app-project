9b8fbd07e977577471bfcfd8954365c6a8d97bf4
Glimmer <hakaishiny@gmail.com>
2026-02-20 19:00:16 +0900

    security: hide keys in .env and remove supabaseClient from tracking

--- frontend/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ctzifgdjkhsfsbvikvii.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0emlmZ2Rqa2hzZnNidmlrdmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyODkyMTMsImV4cCI6MjA4NTg2NTIxM30.fuMBRZM5RxbFCP-phTahn7UScVV5FjbWmRIeCSC_Apg'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
